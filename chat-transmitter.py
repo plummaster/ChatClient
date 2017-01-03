53ff5cf6b04403577d988208d919da0f53c38c3f
plummaster <the.ellen.li@gmail.com>
2017-01-03 10:05:47 -0500

    Updated to allow incoming messages

--- chat-transmitter.py
+++ chat-transmitter.py
@@ -1,9 +1,9 @@
-# Here is a basic program file to get us started.
+# need two threads: receiving and outgoing.
+from threading import Thread
 
 # I think this module may be very helpful for us
 # Documentation is here: https://docs.python.org/3/library/socket.html
 import socket
-
 
 # Initialize the socket
 
@@ -19,21 +19,36 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((host, port)) # Double parens are important to make the argument a tuple.
 serverSocket.listen(0)          # This says not to keep clients queued up in line to connect... I think.
 
+
 # The server socket is just here to accept incoming connections. When it does
 # accept one, it creates a new client socket for the actual communication.
 print("Waiting for a client to connect")
 clientSocket, clientAddress = serverSocket.accept()
 
+def ReplyPrinter(Thread):
+	def __init__(self):
+		Thread.__init__(self)
+	def run(self):
+		While True: 
+			reply = sock.recv(port)
+			print (reply)
+
+thread = ReplyPrinter()
+# thread is 'daemon', background task that wil quit when other threads quit.
+thread.daemon()
+thread.start()
 
 # Once we have a connected receiver, begin broadcasting
 print("Begin typing messages for broadcast.")
 print("Type '/exit' to end the program.")
+# outgoing message
 message = ""
-
+# send message to client
 # Main loop that listens for messages
 while message != "/exit":
 	message = raw_input("message> ")
 	clientSocket.send(message)
+	recvmessage = peer.recv(port)
 	
 # Close the sockets before exiting
 clientSocket.close()
